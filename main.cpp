10b04388c217d3f665b8f1c9c6f7e5ba9c3b4466
Matisimo <matas.remeskevicius@gmail.com>
2020-02-12 11:26:07 +0200

    First program tryout, kinda working

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int N;
+    cout<<"Iveskite studentu skaiciu: \n";
+    while(!(cin >> N))
+    {
+        cout << "Bloga ivestis! Iveskite studentu skaiciu skaitmeniu";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout<<"\n";
+    }
+    string name[N], surname[N];
+    int homeworkCount[N];
+    int homeworkSum[N];
+    int homework[N][1000], exam[N];
+    double finalGrade[N];
+    for(int i=0; i<N; i++)
+    {
+        cout<<"Iveskite savo varda: \n";
+        while(!(cin >> name[i]))
+        {
+            cout << "Bloga ivestis! Iveskite savo varda";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"\n";
+        }
+        cout<<"Iveskite savo pavarde: \n";
+        while(!(cin >> surname[i]))
+        {
+            cout << "Bloga ivestis! Iveskite savo pavarde";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"\n";
+        }
+        cout<<"Iveskite savo namu darbu uzduociu skaiciu: \n";
+        while(!(cin >> homeworkCount[i]))
+        {
+            cout << "Bloga ivestis! Iveskite savo namu darbu uzduociu skaiciu";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"\n";
+        }
+        homeworkSum[i] = 0;
+        for(int j=0; j<homeworkCount[i]; j++)
+        {
+            cout<<"Iveskite savo namu darbu bala: \n";
+            while(!(cin >> homework[i][j]))
+            {
+                cout << "Bloga ivestis! Iveskite savo namu darbu bala skaiciumi";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(),'\n');
+                cout<<"\n";
+            }
+            homeworkSum[i] += homework[i][j];
+        }
+        cout<<"Iveskite savo egzamino bala: \n";
+        while(!(cin >> exam[i]))
+        {
+            cout << "Bloga ivestis! Iveskite savo namu darbu bala skaiciumi";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"\n";
+        }
+        finalGrade[i] = ((homeworkSum[i]*1.0/homeworkCount[i]) * 0.4) + (0.6*exam[i]);
+    }
+
+    cout<<setw(20)<<left<<"Pavarde"<<setw(10)<<left<<"Vardas"<<setw(20)<<"Galutinis (Vid.)"<<endl;
+    cout<<"-------------------------------------------------"<<endl;
+
+
+    for(int i=0; i<N; i++)
+    {
+        cout<<setw(20)<<left<<surname[i]<<setw(10)<<left<<name[i]<<setw(10)<<left<<fixed<<setprecision(2)<<finalGrade[i]<<"\n";
+    }
+
+}
+
