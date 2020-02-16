58fb5f1338d2b5f45edd17fbf0eb8ad25f55605f
Matas <matas.remeskevicius@gmail.com>
2020-02-16 16:05:21 +0200

    Finished the task with mediana

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <limits>
 #include <iomanip>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
     int N;
+
     cout<<"Iveskite studentu skaiciu: \n";
     while(!(cin >> N) || N<0)
     {
@@ -14,11 +16,13 @@ int main()
         cin.ignore(numeric_limits<streamsize>::max(),'\n');
         cout<<"\n";
     }
+
     string name[N], surname[N];
     int homeworkCount[N];
     int homeworkSum[N];
     int homework[N][1000], exam[N];
     double finalGrade[N];
+
     for(int i=0; i<N; i++)
     {
         cout<<"Iveskite savo varda: \n";
@@ -29,6 +33,7 @@ int main()
             cin.ignore(numeric_limits<streamsize>::max(),'\n');
             cout<<"\n";
         }
+
         cout<<"Iveskite savo pavarde: \n";
         while(!(cin >> surname[i]))
         {
@@ -37,6 +42,7 @@ int main()
             cin.ignore(numeric_limits<streamsize>::max(),'\n');
             cout<<"\n";
         }
+
         cout<<"Iveskite savo namu darbu uzduociu skaiciu: \n";
         while(!(cin >> homeworkCount[i]) || homeworkCount[i]<0)
         {
@@ -45,6 +51,7 @@ int main()
             cin.ignore(numeric_limits<streamsize>::max(),'\n');
             cout<<"\n";
         }
+
         homeworkSum[i] = 0;
         for(int j=0; j<homeworkCount[i]; j++)
         {
@@ -59,6 +66,7 @@ int main()
             }
             homeworkSum[i] += homework[i][j];
         }
+
         cout<<"Iveskite savo egzamino bala: \n";
         while(!(cin >> exam[i]) || exam[i] >10 || exam[i]<0)
         {
@@ -68,16 +76,66 @@ int main()
             cout<<"\n";
         }
         finalGrade[i] = ((homeworkSum[i]*1.0/homeworkCount[i]) * 0.4) + (0.6*exam[i]);
+
     }
 
-    cout<<setw(20)<<left<<"Pavarde"<<setw(10)<<left<<"Vardas"<<setw(20)<<"Galutinis (Vid.)"<<endl;
-    cout<<"-------------------------------------------------"<<endl;
+    cout<<"Isvesti vidurki ar mediana? (Vidurkis - v, Mediana - m \n";
+    string temp;
 
-
-    for(int i=0; i<N; i++)
+    cin>>temp;
+    if(temp == "v")
     {
-        cout<<setw(20)<<left<<surname[i]<<setw(10)<<left<<name[i]<<setw(10)<<left<<fixed<<setprecision(2)<<finalGrade[i]<<"\n";
+        cout<<setw(20)<<left<<"Pavarde"<<setw(10)<<left<<"Vardas"<<setw(20)<<"Galutinis (Vid.)"<<endl;
+        cout<<"-------------------------------------------------"<<endl;
+
+
+        for(int i=0; i<N; i++)
+        {
+            cout<<setw(20)<<left<<surname[i]<<setw(10)<<left<<name[i]<<setw(10)<<left<<fixed<<setprecision(2)<<finalGrade[i]<<"\n";
+        }
     }
+    else if( temp == "m")
+    {
+        cout<<setw(20)<<left<<"Pavarde"<<setw(10)<<left<<"Vardas"<<setw(20)<<"Galutinis (Med.)"<<endl;
+        cout<<"-------------------------------------------------"<<endl;
+
+        for(int i=0; i<N; i++)
+        {
+
+            for(int j = 0; j<homeworkCount[i]-1; j++)
+            {
+                for(int k = j+1; k<homeworkCount[i]; k++)
+                {
+                    if(homework[i][j]>homework[i][k])
+                    {
+                        int tempas;
+                        tempas= homework[i][j];
+                        homework[i][j] = homework[i][k];
+                        homework[i][k] = tempas;
+                    }
+                }
+            }
+
+
+            if(homeworkCount[i]%2==1)
+            {
+
+                cout<<setw(20)<<left<<surname[i]<<setw(10)<<left<<name[i]<<setw(10)<<left<<fixed<<setprecision(2)<<homework[i][homeworkCount[i]/2]<<"\n";
+            }
+            else
+            {
+                cout<<setw(20)<<left<<surname[i]<<setw(10)<<left<<name[i]<<setw(10)<<left<<fixed<<setprecision(2)<<(homework[i][homeworkCount[i]/2] + homework[i][(homeworkCount[i]/2)+1])/2.0<<"\n";
+            }
+
+        }
+    }
+
+
+    else
+    {
+        cout<<"Bloga įvestis. \n";
+    }
+
 
 }
 
